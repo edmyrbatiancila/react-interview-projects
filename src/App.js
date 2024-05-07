4be960203d2cde9fb4fc9ebdf5049f9ec7d12b3d
edBatiancila <127021800+edmyrbatiancila@users.noreply.github.com>
2024-05-07 12:13:08 +0000

    Added random color component and adjust its codes and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
 import Accordian from './components/accordian/index';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
       {/* Accordion component */}
-      <Accordian />
+      {/* <Accordian /> */}
+      {/* Random color component */}
+      <RandomColor />
     </div>
   );
 }
