05f461ba1be10e44cc476863679762a074d8d9a6
edBatiancila <127021800+edmyrbatiancila@users.noreply.github.com>
2024-05-07 12:52:47 +0000

    Added StarRating component and update the code and styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Accordian from './components/accordian/index';
 import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       {/* Accordion component */}
       {/* <Accordian /> */}
       {/* Random color component */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      {/* Star Rating Component */}
+      <StarRating />
     </div>
   );
 }
