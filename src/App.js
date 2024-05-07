ab6cdff2a41ce32a54a082b0810ee9e8029ababc
Edmyr <127021800+edmyrbatiancila@users.noreply.github.com>
2024-05-07 08:07:28 +0800

    Second Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Accordian from './components/accordian/index';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Accordion component */}
+      <Accordian />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/accordian/data.js
@@ -0,0 +1,27 @@
+const data = [
+  {
+    id: "1",
+    question: "What is accordion?",
+    answer:
+      "An accordion is a vertically stacked set of interactive headings that each contain a title, content snippet, or thumbnail representing a section of content.",
+  },
+  {
+    id: "2",
+    question: "How to center accordion?",
+    answer: "Simply put everything inside a div and use text-align center.",
+  },
+  {
+    id: "3",
+    question: "How does an accordion work in HTML?",
+    answer:
+      "In UI design, an accordion menu is a vertically stacked list of various pieces of information. For each list, there is a labelled header pointing to corresponding content. Each list's content is hidden by default. Clicking on a particular label will expand its content.",
+  },
+  {
+    id: "4",
+    question: "How to make an accordion responsive?",
+    answer:
+      "data-responsive-accordion-tabs: This attribute is used to make the accordion change from Tabs to List and List to Tabs responsively when the webpage width changes respectively. data-accordion-item: This attribute is used to display the content when the accordion item is clicked.",
+  },
+];
+
+export default data;
