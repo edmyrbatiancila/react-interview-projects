21a9dd685936db0399a7446fd5a77a40eabf2857
edBatiancila <127021800+edmyrbatiancila@users.noreply.github.com>
2024-05-08 05:22:32 +0000

    added tree-view components

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
 import LoadMoreData from './components/load-more-data';
+import TreeView from './components/tree-view';
+import { menus } from './components/tree-view/data';
 
 function App() {
   return (
     <div className="App">
       {/* Accordion Component */}
       {/* <Accordian /> */}
+
       {/* Random Color Component */}
       {/* <RandomColor /> */}
+
       {/* Star Rating Component */}
       {/* <StarRating /> */}
+
       {/* Image Slider Component */}
       {/* <ImageSlider url={'https://picsum.photos/v2/list'} page={"1"} limit={"10"} /> */}
+
       {/* Load more products component */}
-      <LoadMoreData />
+      {/* <LoadMoreData /> */}
+
+      {/* Tree view component */}
+      <TreeView menus={menus} />
     </div>
   );
 }
---
+++ src/components/tree-view/menu-item.jsx
@@ -0,0 +1,15 @@
+import MenuList from "./menu-list";
+
+
+export default function MenuItem({ item }) {
+    return(
+        <li>
+            <p>{item.label}</p>
+            {
+                item && item.children && item.children.length > 0 ?
+                <MenuList />
+                : null
+            }
+        </li>
+    );
+}
