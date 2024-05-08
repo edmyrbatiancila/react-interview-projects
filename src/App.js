e87bce46d2d2815d8b3cfda32f00a1227df38a27
edBatiancila <127021800+edmyrbatiancila@users.noreply.github.com>
2024-05-08 04:46:57 +0000

    Added Load More Product components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Accordian from './components/accordian/index';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 
 function App() {
   return (
@@ -15,7 +16,9 @@ function App() {
       {/* Star Rating Component */}
       {/* <StarRating /> */}
       {/* Image Slider Component */}
-      <ImageSlider url={'https://picsum.photos/v2/list'} page={"1"} limit={"10"} />
+      {/* <ImageSlider url={'https://picsum.photos/v2/list'} page={"1"} limit={"10"} /> */}
+      {/* Load more products component */}
+      <LoadMoreData />
     </div>
   );
 }
